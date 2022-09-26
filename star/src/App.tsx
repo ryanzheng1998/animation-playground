536ffcd1f51fac082b96475f348190714e5fd1b7
ryan-zheng <ryanzheng1998@gmail.com>
2022-09-26 10:32:56 +0800

    finished star

--- star/src/App.tsx
+++ star/src/App.tsx
@@ -1,5 +1,38 @@
+import { animated, config, useSpring } from '@react-spring/web'
+import React from 'react'
+
 function App() {
-  return <p className="bg-red-200">Hello World</p>
+  const [flip, setFlip] = React.useState(false)
+
+  const styles = useSpring({
+    from: { length: 0 },
+    to: { length: 1 },
+    reverse: flip,
+    onRest: () => setFlip((x) => !x),
+    config: config.molasses,
+  })
+
+  return (
+    <div className="fixed inset-0 grid place-items-center">
+      <animated.svg
+        className="w-80 aspect-square"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+        strokeDasharray={100}
+        strokeDashoffset={styles.length.to((x) => x * 100)}
+      >
+        <title>Star</title>
+        <path
+          d="M480 208H308L256 48l-52 160H32l140 96-54 160 138-100 138 100-54-160z"
+          fill="none"
+          stroke="currentColor"
+          stroke-linejoin="round"
+          stroke-width="1"
+          pathLength={100}
+        />
+      </animated.svg>
+    </div>
+  )
 }
 
 export default App
