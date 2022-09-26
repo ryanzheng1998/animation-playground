1bd35aef6593d76ba039be8456c22347d043fdb7
ryan-zheng <ryanzheng1998@gmail.com>
2022-09-26 16:38:27 +0800

    init

--- animating-auto/src/App.tsx
+++ animating-auto/src/App.tsx
@@ -1,5 +1,32 @@
+import { animated, useSpring } from '@react-spring/web'
+import React from 'react'
+
 function App() {
-  return <p className="bg-red-200">Hello World</p>
+  const [flip, setFlip] = React.useState(false)
+  const { number } = useSpring({
+    from: { number: 0 },
+    to: { number: 200 },
+    reverse: flip,
+  })
+
+  return (
+    <div className="fixed inset-0 grid place-items-center">
+      <div
+        onClick={() => {
+          setFlip((x) => !x)
+        }}
+        className="border border-black rounded overflow-hidden relative grid place-items-center w-52 h-12 cursor-pointer"
+      >
+        <animated.div
+          className="absolute bg-pink-400 inset-0"
+          style={{ scaleX: number.to((x) => x / 200), transformOrigin: 'left' }}
+        />
+        <animated.p className="absolute">
+          {number.to((x) => x.toFixed(0))}
+        </animated.p>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- loop/src/App.tsx
+++ loop/src/App.tsx
@@ -1,5 +1,20 @@
+import { animated, useSpring } from '@react-spring/web'
+
 function App() {
-  return <p className="bg-red-200">Hello World</p>
+  const { rotateZ } = useSpring({
+    from: { rotateZ: 0 },
+    to: { rotateZ: 90 },
+    loop: true,
+  })
+
+  return (
+    <div className="fixed inset-0 grid place-items-center">
+      <animated.div
+        style={{ rotateZ }}
+        className="bg-green-300 w-28 aspect-square rounded"
+      />
+    </div>
+  )
 }
 
 export default App
