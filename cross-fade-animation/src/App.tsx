6ac971cc982adc6eabf1602c98afc2189fdacdd5
ryan-zheng <ryanzheng1998@gmail.com>
2022-09-29 17:13:07 +0800

    complete

--- cross-fade-animation/src/App.tsx
+++ cross-fade-animation/src/App.tsx
@@ -1,5 +1,51 @@
+import { a, useTransition } from '@react-spring/web'
+import React from 'react'
+
 function App() {
-  return <p className="bg-red-200">Hello World</p>
+  const [show, setShow] = React.useState(false)
+
+  const transitions = useTransition(show, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  })
+
+  const content = transitions((styles, item) => {
+    if (item) {
+      return (
+        <a.div
+          className="absolute h-full w-full bg-green-400"
+          style={{ ...styles, mixBlendMode: 'plus-lighter' }}
+        />
+      )
+    }
+
+    return (
+      <a.div
+        className="absolute h-full w-full bg-green-400"
+        style={{ ...styles, mixBlendMode: 'plus-lighter' }}
+      />
+    )
+  })
+
+  return (
+    <>
+      <div
+        style={{ isolation: 'isolate' }}
+        className="fixed inset-0 grid place-items-center"
+      >
+        {content}
+      </div>
+      <button
+        onClick={() => {
+          setShow((x) => !x)
+        }}
+        className="fixed top-3 left-3"
+      >
+        Toggle
+      </button>
+    </>
+  )
 }
 
 export default App
