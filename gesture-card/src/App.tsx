91ef21c4b37a14f030516b3c051b2b9ade4f8261
ryan-zheng <ryanzheng1998@gmail.com>
2022-10-17 12:59:11 +0800

    fix gesture card

--- gesture-card/src/App.tsx
+++ gesture-card/src/App.tsx
@@ -1,26 +1,29 @@
-import { animated, useSpring } from '@react-spring/web'
+import { animated, config, to, useSpring } from '@react-spring/web'
 import React from 'react'
 import pic from './assets/react.svg'
 
 function App() {
   const [props, api] = useSpring(() => ({
-    transform: 'perspective(1000px)',
-    rotateX: 0,
-    rotateY: 0,
-    rotateZ: 0,
-    scale: 1,
     x: 0,
     y: 0,
+    rotateX: 0,
+    rotateY: 0,
+    scale: 1,
+    config: config.gentle,
   }))
 
   const [mouseDownPosition, setMouseDownPosition] =
     React.useState<DOMPoint | null>(null)
+
+  const [mouseInsideElement, setMouseInsideElement] = React.useState(false)
 
   const elementRef = React.useRef<HTMLDivElement>(null)
 
   React.useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
       if (mouseDownPosition === null) return
+
+      document.body.style.cursor = 'grabbing'
 
       const element = elementRef.current
 
@@ -29,16 +32,26 @@ function App() {
       const x = e.pageX - element.offsetLeft - mouseDownPosition.x
       const y = e.pageY - element.offsetTop - mouseDownPosition.y
 
-      console.log(e.pageX, element.offsetLeft, mouseDownPosition.x)
-
       api.start({
         x,
         y,
+        rotateX: 0,
+        rotateY: 0,
       })
     }
 
     const handleMouseUp = () => {
       setMouseDownPosition(null)
+
+      document.body.style.cursor = 'auto'
+
+      if (!mouseInsideElement) {
+        api.start({
+          scale: 1,
+          rotateX: 0,
+          rotateY: 0,
+        })
+      }
     }
 
     window.addEventListener('mousemove', handleMouseMove)
@@ -48,34 +61,39 @@ function App() {
       window.removeEventListener('mousemove', handleMouseMove)
       window.removeEventListener('mouseup', handleMouseUp)
     }
-  }, [mouseDownPosition])
+  }, [mouseDownPosition, mouseInsideElement, api])
 
   return (
     <div className="fixed inset-0 grid place-items-center">
       <animated.div
         ref={elementRef}
         onMouseEnter={() => {
-          // api.start({ scale: 1.2 })
+          api.start({ scale: 1.1 })
+          setMouseInsideElement(true)
         }}
-        onMouseLeave={(e) => {
+        onMouseLeave={() => {
+          setMouseInsideElement(false)
+          if (mouseDownPosition !== null) return
           api.start({ scale: 1, rotateX: 0, rotateY: 0 })
         }}
         onMouseMove={(e) => {
+          if (mouseDownPosition !== null) return
           const element = e.currentTarget
-          // rotate section
-          // {
-          //   const x = e.pageX - element.offsetLeft - element.clientWidth / 2
-          //   const y = e.pageY - element.offsetTop - element.clientHeight / 2
+          const x = e.pageX - element.offsetLeft - element.clientWidth / 2
+          const y = e.pageY - element.offsetTop - element.clientHeight / 2
 
-          //   // rotateX: 0 -> 30
-          //   const xDeg = (x / element.clientWidth / 2) * 30
-          //   const yDeg = (y / element.clientHeight / 2) * -30
+          // consider translate
+          const nextX = x - props.x.get()
+          const nextY = y - props.y.get()
 
-          //   api.start({
-          //     rotateX: yDeg,
-          //     rotateY: xDeg,
-          //   })
-          // }
+          // rotateX: 0 -> 30
+          const xDeg = (nextX / element.clientWidth / 2) * 30
+          const yDeg = (nextY / element.clientHeight / 2) * -30
+
+          api.start({
+            rotateX: yDeg,
+            rotateY: xDeg,
+          })
         }}
         onMouseDown={(e) => {
           const element = e.currentTarget
@@ -84,8 +102,15 @@ function App() {
 
           setMouseDownPosition(new DOMPoint(x, y))
         }}
-        className="shadow-xl cursor-grab w-72 aspect-square rounded bg-green-400"
-        style={props}
+        className="shadow-xl cursor-grab w-36 aspect-square rounded bg-green-400"
+        style={{
+          transform: to(
+            [props.x, props.y, props.rotateX, props.rotateY, props.scale],
+            (x, y, rotateX, rotateY, scale) => {
+              return `perspective(600px) translate(${x}px, ${y}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(${scale})`
+            }
+          ),
+        }}
       >
         <img className="w-full h-full" src={pic} draggable={false} />
       </animated.div>
