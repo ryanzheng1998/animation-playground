36fd8b5fc27c01c72790265bc10a3e0b51159d57
ryan-zheng <ryanzheng1998@gmail.com>
2022-12-26 22:01:01 +0800

    progress

--- draggable-list/src/App.tsx
+++ draggable-list/src/App.tsx
@@ -1,129 +1,129 @@
 import { a, useSprings } from '@react-spring/web'
-import { useEffect, useRef } from 'react'
 
-const list = 'hello world this is a list'
-  .split(' ')
-  .map((x) => ({ value: x, key: crypto.randomUUID() }))
+const list = [
+  { value: 'hello', color: 'red' },
+  { value: 'world', color: 'blue' },
+  { value: 'this', color: 'green' },
+  { value: 'is', color: 'yellow' },
+  { value: 'a', color: 'purple' },
+  { value: 'list', color: 'orange' },
+]
 
 function App() {
   const [springs, api] = useSprings(list.length, () => {
     return {
       y: 0,
       z: 0,
-      zIndex: 0,
       scale: 1,
+      boxShadow: '0 0px 0px 0 rgba(0, 0, 0, 0)',
     }
   })
 
-  const elementInfo = useRef<{ ref: HTMLDivElement; order: number }[]>([])
-  const rects = useRef<DOMRect[]>([])
-
-  // sync with element's position
-  useEffect(() => {
-    const update = () => {
-      rects.current = elementInfo.current.map((info) => {
-        return info.ref.getBoundingClientRect()
-      })
-    }
-
-    update()
-
-    window.addEventListener('resize', update)
-
-    return () => {
-      window.removeEventListener('resize', update)
-    }
-  }, [])
-
   return (
-    <div className="grid inset-0 place-items-center">
+    <div
+      className="grid inset-0 place-items-center"
+      style={{ transformStyle: 'preserve-3d' }}
+    >
       {list.map((x, i) => {
         const props = springs[i]!
 
         return (
           <a.div
-            key={x.key}
-            className="bg-green-300 rounded w-32 py-2 text-center m-1 cursor-grab active:cursor-grabbing select-none"
-            style={{ ...props }}
-            ref={(ref) => {
-              elementInfo.current[i] = {
-                ref: ref!,
-                order: i,
-              }
-            }}
+            key={i}
+            className="rounded w-32 py-2 text-center m-1 cursor-grab active:cursor-grabbing select-none"
+            style={{ ...props, background: x.color }}
             onPointerDown={(e) => {
               api.start((i2) => {
                 if (i !== i2) return
                 return {
                   scale: 1.1,
                   z: 10,
-                  zIndex: 10,
+                  boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
                 }
               })
 
-              const mouseDownY = e.pageY - props.y.get()
+              const mouseDownY = e.pageY - props.y.get() // this is for dragging
               const element = e.target as HTMLDivElement
               element.setPointerCapture(e.pointerId)
+              const elements = [
+                ...element.parentNode!.children,
+              ] as HTMLDivElement[]
+
+              const getClosestElementYPosition = (y: number) => {
+                return (
+                  elements.find((el, i) => {
+                    const nextElement = elements[i + 1]
+                    const nextElementYPosition =
+                      nextElement?.offsetTop ?? Infinity
+
+                    return y > el.offsetTop && y < nextElementYPosition
+                  })?.offsetTop ?? elements[0]!.offsetTop
+                )
+              }
 
               const onPointerMove = (e: PointerEvent) => {
-                const y = e.pageY - mouseDownY
-
-                // update the order
-                const currentOrder = elementInfo.current[i].order
-                const targetRect = rects.current[i]
-                const newOrder = rects.current.findIndex((rect) => {
-                  const newY = y + targetRect.y
-                  return newY > rect.y && newY < rect.y + rect.height
-                })
-                const swapOrder =
-                  elementInfo.current.find((x) => x.order === newOrder)
-                    ?.order ?? currentOrder
-
-                elementInfo.current = elementInfo.current.map((x, i2) => {
-                  if (i2 === i) {
-                    return {
-                      ...x,
-                      order: swapOrder,
-                    }
-                  }
-
-                  if (x.order === newOrder) {
-                    return {
-                      ...x,
-                      order: currentOrder,
-                    }
-                  }
-
-                  return {
-                    ...x,
-                  }
-                })
-
+                // drag element
                 api.start((i2) => {
-                  if (i !== i2) {
-                    const order = elementInfo.current[i2].order
-
-                    return {
-                      y: rects.current[order].y - rects.current[i2].y,
-                    }
-                  }
-
+                  if (i !== i2) return
                   return {
-                    y,
+                    y: e.pageY - mouseDownY,
                     immediate: true,
                   }
                 })
+
+                // update swap element
+                // change to find empty slot to swap with
+                {
+                  const yPosition = getClosestElementYPosition(e.pageY)
+                  const yPositions = elements.map((el, i) => {
+                    const goal = springs[i]!.y.goal
+                    return getClosestElementYPosition(goal + el.offsetTop)
+                  })
+                  const emptySlot = elements.find((el) => {
+                    return yPositions.some((x) => x === el.offsetTop)
+                  })
+
+                  api.start((i2) => {
+                    if (i === i2) return
+                    if (emptySlot === undefined) return
+
+                    const emptyYPosition = emptySlot.offsetTop
+
+                    const currentYPosition = yPositions[i2]!
+
+                    if (emptyYPosition > yPosition) {
+                      console.log(currentYPosition, emptyYPosition)
+                      if (
+                        currentYPosition > emptyYPosition ||
+                        yPosition < emptyYPosition
+                      )
+                        return
+
+                      const positionIndex = elements.findIndex(
+                        (el) => el.offsetTop === currentYPosition
+                      )
+
+                      return {
+                        y: elements[positionIndex]!.offsetTop,
+                      }
+                    }
+
+                    return {}
+                  })
+                }
               }
 
               const onPointerUp = (e: PointerEvent) => {
                 api.start((i2) => {
-                  const order = elementInfo.current[i2].order
+                  if (i !== i2) return
+
+                  const yPosition = getClosestElementYPosition(e.pageY)
 
                   return {
-                    y: rects.current[order].y - rects.current[i2].y,
+                    y: yPosition - element.offsetTop,
                     z: 0,
-                    zIndex: 0,
                     scale: 1,
+                    boxShadow: '0 0px 0px 0 rgba(0, 0, 0, 0)',
                   }
                 })
                 element.releasePointerCapture(e.pointerId)
