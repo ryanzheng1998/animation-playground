68a14b3a30653b8a98509042bdfb9c638e7cfa9f
ryan-zheng <ryanzheng1998@gmail.com>
2023-02-18 09:25:56 +0800

    disable pinch zoom on specific element

--- disable-pinch-zoom/src/App.tsx
+++ disable-pinch-zoom/src/App.tsx
@@ -1,19 +1,36 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 function App() {
+  const elementRef = useRef<HTMLDivElement | null>(null)
+
   useEffect(() => {
+    const element = elementRef.current
+    if (element === null) return
+
     const onWheel = (e: WheelEvent) => {
       e.preventDefault()
     }
 
-    window.addEventListener('wheel', onWheel, { passive: false })
+    element.addEventListener('wheel', onWheel, { passive: false })
 
     return () => {
-      window.removeEventListener('wheel', onWheel)
+      element.removeEventListener('wheel', onWheel)
     }
   }, [])
 
-  return <div className="w-9 aspect-square bg-green-400"></div>
+  const longElement = new Array(100).fill(0).map((_, i) => {
+    return <p key={i}>{i}</p>
+  })
+
+  return (
+    <>
+      <div
+        ref={elementRef}
+        className="w-32 aspect-square bg-green-400 overflow-auto"
+      ></div>
+      {longElement}
+    </>
+  )
 }
 
 export default App
