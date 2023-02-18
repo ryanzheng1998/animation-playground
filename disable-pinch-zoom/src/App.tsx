1c2f467b5da40f138f9b727b5080317eca16382a
ryan-zheng <ryanzheng1998@gmail.com>
2023-02-18 09:13:37 +0800

    slove pinch zoom

--- disable-pinch-zoom/src/App.tsx
+++ disable-pinch-zoom/src/App.tsx
@@ -1,40 +1,19 @@
-import { ReactNode, useState } from 'react'
-import { createPortal } from 'react-dom'
+import { useEffect } from 'react'
 
-export const IFrame = <
-  T extends {
-    children: ReactNode
-  } & React.IframeHTMLAttributes<HTMLIFrameElement>
->({
-  children,
-  ...props
-}: T) => {
-  const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null)
-  const mountNode = contentRef?.contentWindow?.document?.body
-
-  return (
-    <iframe {...props} ref={setContentRef}>
-      {mountNode && createPortal(children, mountNode)}
-    </iframe>
-  )
-}
 function App() {
-  return (
-    <div>
-      <p>Test</p>
-      <IFrame
-        width={100}
-        height={100}
-        style={{ pointerEvents: 'none', background: 'black' }}
-        sandbox="allow-scripts"
-      >
-        <div
-          className="bg-green-500 pointer-events-none w-72 h-72"
-          style={{ width: 60, height: 60, background: 'green' }}
-        />
-      </IFrame>
-    </div>
-  )
+  useEffect(() => {
+    const onWheel = (e: WheelEvent) => {
+      e.preventDefault()
+    }
+
+    window.addEventListener('wheel', onWheel, { passive: false })
+
+    return () => {
+      window.removeEventListener('wheel', onWheel)
+    }
+  }, [])
+
+  return <div className="w-9 aspect-square bg-green-400"></div>
 }
 
 export default App
